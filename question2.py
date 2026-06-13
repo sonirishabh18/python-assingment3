2d4759622e5e90bf7afc49ecf71d1f0c10c5bd10
sonirishabh18 <sonyrishabh18@gmail.com>
2026-06-13 21:45:27 +0530

    Add Car class with attributes and instances

---
+++ question2.py
@@ -0,0 +1,11 @@
+class Car:
+
+    brand = "Toyota"
+    model = "Fortuner"
+    price = 5000000
+
+c1 = Car()
+c2 = Car()
+
+print(c1.brand, c1.model, c1.price)
+print(c2.brand, c2.model, c2.price)
